bdae695f5b23ff3c6ed8511eb7ee497e9a068d85
marekchen <marek@163.com>
2017-06-05 10:05:08 +0800

    update for 1.1.009 release

--- app/src/main/java/com/droi/sdk/droifeedbackdemo/MainActivity.java
+++ app/src/main/java/com/droi/sdk/droifeedbackdemo/MainActivity.java
@@ -2,7 +2,6 @@ package com.droi.sdk.droifeedbackdemo;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,9 +9,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.droi.sdk.DroiError;
 import com.droi.sdk.core.FeedbackCoreHelper;
 import com.droi.sdk.feedback.DroiFeedback;
+import com.droi.sdk.feedback.DroiFeedbackError;
+import com.droi.sdk.feedback.DroiFeedbackInfo;
+import com.droi.sdk.feedback.DroiFeedbackReplyListener;
+import com.droi.sdk.feedback.DroiFeedbackSendListener;
+import com.droi.sdk.internal.DroiLog;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -22,24 +32,23 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final Context mContext = this;
         this.setContentView(R.layout.droi_example_activity_main);
-        Log.i("FeedbackCoreHelper", FeedbackCoreHelper.getDeviceId());
+        Log.i(TAG, FeedbackCoreHelper.getDeviceId());
         TextView appId = (TextView) findViewById(R.id.app_id);
         appId.setText(FeedbackCoreHelper.getAppId());
         Button feedback = (Button) findViewById(R.id.feedback);
         feedback.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DroiFeedback.callFeedback(mContext.getApplicationContext());
+                DroiFeedback.callFeedback(MainActivity.this);
             }
         });
         Button userDefined = (Button) findViewById(R.id.user_defined);
         userDefined.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DroiFeedback.setTitleBarColor(0xFFFF9900);
-                DroiFeedback.setSendButtonColor(0xFFFF9900, 0xFFFF9900);
+                DroiFeedback.setTitleBarColor(0xFFFFC125);
+                DroiFeedback.setSendButtonColor(0xFFEEB422, 0xFFFFC125);
             }
         });
         Button setDefault = (Button) findViewById(R.id.set_default);
@@ -58,6 +67,67 @@ public class MainActivity extends Activity {
                 showDialog();
             }
         });
+
+        Button sendButton = (Button) findViewById(R.id.send);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<File> fileList = new ArrayList<>();
+                // 修改为您自己的图片地址
+                fileList.add(new File("/storage/emulated/0/DCIM/.thumbnails/1467103627184.jpg"));
+                fileList.add(new File("/storage/emulated/0/DCIM/.thumbnails/1482126861117.jpg"));
+                DroiFeedback.submitFeedbackInBackground("13333333333", "内容", fileList, new DroiFeedbackSendListener() {
+                    @Override
+                    public void onReturned(DroiError droiError) {
+                        switch (droiError.getCode()) {
+                            case DroiFeedbackError.OK:
+                                Toast.makeText(getApplicationContext(), "发送成功", Toast.LENGTH_SHORT).show();
+                                break;
+                            case DroiFeedbackError.FEEDBACK_NOT_FOUND:
+                                Toast.makeText(getApplicationContext(), "联系方式或反馈内容为空", Toast.LENGTH_SHORT).show();
+                                break;
+                            case DroiFeedbackError.CONTACT_INVALID:
+                                Toast.makeText(getApplicationContext(), "联系方式不合法", Toast.LENGTH_SHORT).show();
+                                break;
+                            case DroiFeedbackError.UPLOAD_IMAGE_FAILED:
+                                Toast.makeText(getApplicationContext(), "文件上传失败", Toast.LENGTH_SHORT).show();
+                                break;
+                            default:
+                                Toast.makeText(getApplicationContext(), "发生错误", Toast.LENGTH_SHORT).show();
+                                Log.e(TAG, droiError.toString());
+                                break;
+                        }
+                    }
+                });
+            }
+        });
+
+        Button getButton = (Button) findViewById(R.id.get);
+        getButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DroiFeedback.getReplyInBackground(new DroiFeedbackReplyListener() {
+                    @Override
+                    public void onResult(DroiError droiError, List<DroiFeedbackInfo> list) {
+                        if (droiError.isOk()) {
+                            if (list != null && list.size() != 0) {
+                                for (DroiFeedbackInfo info : list) {
+                                    String image = "";
+                                    List<String> imageList = info.getImageList();
+                                    for (String imageString : imageList) {
+                                        image += imageString;
+                                    }
+                                    Log.i(TAG, "image:" + image);
+                                }
+                            }
+                        } else {
+                            Log.i(TAG, droiError.toString());
+                        }
+                    }
+                });
+            }
+        });
+
     }
 
     @Override
--- app/src/main/java/com/droi/sdk/droifeedbackdemo/MyApplication.java
+++ app/src/main/java/com/droi/sdk/droifeedbackdemo/MyApplication.java
@@ -4,12 +4,20 @@ import android.app.Application;
 
 import com.droi.sdk.core.Core;
 import com.droi.sdk.feedback.DroiFeedback;
+import com.droi.sdk.feedback.GlideEngine;
+import com.squareup.leakcanary.LeakCanary;
 
 public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return;
+        }
+        LeakCanary.install(this);
         Core.initialize(this);
         DroiFeedback.initialize(this);
+        DroiFeedback.setApiKey("_Alo_HOvf32ohDFdYVKz05DGhCYTpqPzKq78elkEx1iDjmOtPe_anOHQGA2TS4m9");
+        DroiFeedback.setImageEngine(new GlideEngine());
     }
 }
